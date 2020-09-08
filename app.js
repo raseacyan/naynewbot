86c6ff1b0d08452d1a2d987e80ed450c44d27cf9
raseacyan <caesar821009@gmail.com>
2020-09-08 16:26:17 +0630

    hospital

--- app.js
+++ app.js
@@ -559,7 +559,31 @@ const showDoctor = (sender_psid) => {
                   "payload": "James Smith",
                 },               
               ],
-          }]
+          },{
+            "title": "Kenneth Martinez",
+            "subtitle": "General Surgeon",
+            "image_url":"https://image.freepik.com/free-vector/doctor-icon-avatar-white_136162-58.jpg",                       
+            "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Kenneth Martinez",
+                  "payload": "Kenneth Martinez",
+                },               
+              ],
+          },{
+            "title": "Barbara Young",
+            "subtitle": "General Surgeon",
+            "image_url":"https://cdn.iconscout.com/icon/free/png-512/doctor-567-1118047.png",                       
+            "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Barbara Young",
+                  "payload": "Barbara Young",
+                },               
+              ],
+          }
+
+          ]
         }
       }
     }
