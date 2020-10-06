e235da63346eb99cbbee10c8de313d97d28a2828
raseacyan <caesar821009@gmail.com>
2020-10-06 20:04:08 +0630

    test

--- app.js
+++ app.js
@@ -94,21 +94,18 @@ app.post('/webhook', (req, res) => {
 
       let webhook_event = entry.messaging[0];
       let sender_psid = webhook_event.sender.id; 
-
       
 
       sess = req.session;
       if(!sess.user_id){
         sess.user_id =  sender_psid;
-      }      
-
-     
+      }           
 
       if (webhook_event.message) {
         if(webhook_event.message.quick_reply){
             handleQuickReply(sender_psid, webhook_event.message.quick_reply.payload);
           }else{
-            handleMessage(sender_psid, webhook_event.message);                       
+            handleMessage(sender_psid, webhook_event.message, req);                       
           }                
       } else if (webhook_event.postback) {        
         handlePostback(sender_psid, webhook_event.postback);
@@ -417,11 +414,13 @@ function handleQuickReply(sender_psid, received_message) {
 Function to Handle when user send text message
 ***********************************************/
 
-const handleMessage = (sender_psid, received_message) => {
+const handleMessage = (sender_psid, received_message, req) => {
 
   console.log('TEXT REPLY', received_message);
-  //let message;
+ 
   let response;
+  sess = req.session;
+
 
   if(received_message.attachments){
      handleAttachments(sender_psid, received_message.attachments);
