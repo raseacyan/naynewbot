f6af6c3390f1b696bb9c53ac12091d8215dd431f
raseacyan <caesar821009@gmail.com>
2020-10-20 08:22:02 +0630

    Test

--- app.js
+++ app.js
@@ -233,53 +233,6 @@ app.get('/hall', async (req,res) => {
     
 });
 
-
-app.get('/groups', async (req,res) => {  
-
-  sess = req.session;
-
-  if(!sess.student_id){
-     res.redirect('mcc');
-  }  
-
-  const groupsRef = db.collection('groups').orderBy('created_on', 'desc');
-  const snapshot = await groupsRef.get();
-
-  if (snapshot.empty) {
-    res.send('no groups');
-  }else{
-     
-    let groups = [];
-    
-    snapshot.forEach(async(doc) => {
-      let group = {};
-      
-      group = doc.data();
-      group.id = doc.id;
-      
-      let d = new Date(doc.data().created_on._seconds);
-      d = d.toString();
-      group.created_on = d;    
-
-      groups.push(group); 
-       
-    }); 
-
-    let current_student = {
-            id : sess.student_id,
-            name : sess.student_name
-      }
-
-      console.log('GROUPs:', groups);     
-
-    res.render('mcc/groups.ejs', {groups:groups, current_student:current_student});
-    
-  }
-    
-});
-
-
-
 app.get('/groups', async (req,res) => {  
 
   sess = req.session;
@@ -297,13 +250,13 @@ app.get('/groups', async (req,res) => {
             name : sess.student_name
     }  
 
-    console.log('0 Groups');   
+   
 
     res.render('mcc/groups.ejs', {groups:0, current_student:current_student});
 
   }else{
 
-    console.log('Has Groups'); 
+     
      
     let groups = [];
     
